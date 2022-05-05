f2db7b3cc732a9f8a203b3302faa9010310e3e00
frostycoles <dcfrostad@gmail.com>
2022-05-05 15:47:39 -0500

    Adds script back to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app>
+    <router-view />
+  </v-app>
 </template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
