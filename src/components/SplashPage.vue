b5a94c07e7eb46f7c4fd1239e4be6c625136a327
frostycoles <dcfrostad@gmail.com>
2022-05-05 16:55:46 -0500

    Splash page content

--- src/components/SplashPage.vue
+++ src/components/SplashPage.vue
@@ -1,7 +1,41 @@
 <template>
-  <v-container>
+  <v-main>
 
-  </v-container>
+    <v-container
+      class="fill-height"
+    >
+      <v-app-bar
+        absolute
+        color="grey"
+      >
+        
+        <v-toolbar-title>
+          Charity Golf Solutions
+        </v-toolbar-title>
+        
+        <v-spacer></v-spacer>
+
+        <v-btn>Contact Us!</v-btn>
+
+      </v-app-bar>
+
+      <div class="text-h3" color="grey">Your fairway to fundraising</div>
+
+    </v-container>
+
+    <v-container>
+
+      <div class="mb-12 text-h4 text-center">Who We Are</div>
+
+      <div class="mb-6 text-body-1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+
+      <div class="mb-6 text-body-1">It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
+
+      <div class="mb-6 text-body-1">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+
+    </v-container>
+
+  </v-main>
 </template>
 
 <script>
