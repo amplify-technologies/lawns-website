c0b98285147e0d3a224adf274637be6915979923
frostycoles <dcfrostad@gmail.com>
2022-05-16 11:41:57 -0500

    Move AppBar to its own component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <v-app>
+
+    <app-bar />
+
     <router-view />
+    
   </v-app>
 </template>
 
 <script>
+import AppBar from './components/AppBar.vue';
 
 export default {
+  components: { AppBar },
   name: 'App',
 
   data: () => ({
---
+++ src/components/AppBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app-bar
+        absolute
+        color="transparent"
+        elevation=0
+      >
+        <v-img
+          :src="require('@/assets/logo.jpg')"
+          max-height="60"
+          contain
+          position="left"
+        ></v-img>
+        
+        <v-spacer></v-spacer>
+
+        <v-btn
+          text
+          color="white"
+        >Features</v-btn>
+
+        <v-btn
+          text
+          color="white"
+        >Pricing</v-btn>
+
+        <v-btn
+          text
+          color="white"
+        >Sign In</v-btn>
+
+        <v-btn>Try for free</v-btn>
+
+      </v-app-bar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
