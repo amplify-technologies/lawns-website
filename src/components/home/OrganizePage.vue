901f30dfb6f6da1fa9ff83a86b7ba170ce215fcb
jothon <josh@inkbird.io>
2022-05-17 14:59:57 -0500

    Spacing improvemts

--- src/components/home/OrganizePage.vue
+++ src/components/home/OrganizePage.vue
@@ -6,12 +6,12 @@
 
         <v-img
             :src="require('@/assets/organize.png')"
-            max-height="1100"
+            max-height="1200"
         >
             <v-row no-gutters>
                 <v-col
                     cols="6"
-                    class="d-flex flex-column justify-center"
+                    class="d-flex flex-column justify-center align-center"
                 >   
                     <div :class="$vuetify.breakpoint.mdAndDown? 'pl-6' : 'pl-16'">
                         <v-card-title class="text-h5 font-weight-bold">
@@ -29,7 +29,7 @@
                     <v-img
                         :src="require('@/assets/browser_angled.png')"
                         width="400"
-                        class="mt-10"
+                        :style="`transform: ${$vuetify.breakpoint.mdAndDown? 'translate(20px, 0px) scale(1.2);' : 'translate(0px, 100px) scale(1.4);'}`"
                     />
                 </v-col>
             </v-row>
@@ -41,6 +41,8 @@
                     <v-img
                         :src="require('@/assets/phone.png')"
                         width="400"
+                        :style="`transform: ${$vuetify.breakpoint.mdAndDown? 'translate(-10px, -30px) scale(1.1);' : 'translate(0px, 0px) scale(1.2);'}`"
+
                     />
                 </v-col>
                 <v-col
