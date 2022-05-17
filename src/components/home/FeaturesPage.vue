9237ff170f284b76dba76551145cd517e72d908e
jothon <josh@inkbird.io>
2022-05-17 17:56:48 -0500

    adds Funtional carousel with every feature accessable

--- src/components/home/FeaturesPage.vue
+++ src/components/home/FeaturesPage.vue
@@ -35,6 +35,7 @@
                     outlined
                     class="text-capitalize"
                     x-large
+                    @click="carousel=0"
                   >
                     Job Scheduler
                     <v-spacer></v-spacer>
@@ -48,6 +49,7 @@
                     outlined
                     class="text-capitalize"
                     x-large
+                    @click="carousel=1"
                   >
                     In App Messages
                     <v-spacer></v-spacer>
@@ -61,6 +63,7 @@
                     outlined
                     class="text-capitalize"
                     x-large
+                    @click="carousel=2"
                   >
                     Invoicing
                     <v-spacer></v-spacer>
@@ -74,6 +77,7 @@
                     outlined
                     class="text-capitalize"
                     x-large
+                    @click="carousel=3"
                   >
                     Easy Transaction
                     <v-spacer></v-spacer>
@@ -87,6 +91,7 @@
                     outlined
                     class="text-capitalize"
                     x-large
+                    @click="carousel=4"
                   >
                     Fast Collections
                     <v-spacer></v-spacer>
@@ -100,6 +105,7 @@
                     outlined
                     class="text-capitalize"
                     x-large
+                    @click="carousel=5"
                   >
                     Organize Clients
                     <v-spacer></v-spacer>
@@ -111,62 +117,71 @@
 
           </v-card>
         </v-col>
+
         <v-col
           md="4"
         >
-          <v-card
-            flat
-            color="transparent"
-            class="pt-16 text-center"
+          <v-carousel
+            vertical
+            :show-arrows="false"
+            hide-delimiter-background
+            hide-delimiters
+            v-model="carousel"
           >
 
-            <p
-              class="text-h5 mb-2 font-weight-bold"
+            <v-carousel-item
+              v-for="(item, i) in features"
+              :key="i"
             >
-              Scheduler
-            </p>
+              <p class="text-h5 mb-2 font-weight-bold">
+                {{ item.title }}
+              </p>
+              <p class="text-body-1 font-weight-medium">
+                {{ item.desc }}
+              </p>
+            </v-carousel-item>
 
-            <p
-              class="text-body-1 font-weight-medium"
-            >
-              Keep track of all jobs on a visual calendar, edit details and see who's doing what.
-            </p>
-
-
-          </v-card>
-        </v-col>
-
-      </v-row>
-      <v-row>
-        <v-col
-          offset-md="8"
-          justify="center"
-        >
-          <v-card
-            class="text-center"
-            flat
-            color="transparent"
-          >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="grey lighten-1"
-                class="white--text text-caption font-weight-bold text-capitalize"
-              >
-                Hover to read about each feature
-                <v-icon>mdi-gesture-tap</v-icon>
-              </v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
+          </v-carousel>
         </v-col>
       </v-row>
+
     </v-container>
   </v-img>
 </template>
 
 <script>
 export default {
-  name: "featuresPage"
+  name: "featuresPage",
+  data() {
+    return {
+      features: {
+        scheduler: {
+          title: "Scheduler",
+          desc: "Keep track of all jobs on a visual calendar, edit details and see who's doing what."
+        },
+        in_app_msg: {
+          title: "In App Messages",
+          desc: "s"
+        },
+        invoicing: {
+          title: "Invoicing",
+          desc: "a"
+        },
+        easy_trans: {
+          title: "Easy Transaction",
+          desc: "f"
+        },
+        fast_col: {
+          title: "Fast Collections",
+          desc: "e"
+        },
+        organize: {
+          title: "Organize Clients",
+          desc: "4"
+        }  
+      },
+      carousel: 0
+    }
+  }
 }
 </script>
