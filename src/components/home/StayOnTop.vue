c40f73fbba6e9852b5b811596d2a71096a520ae0
frostycoles <dcfrostad@gmail.com>
2022-05-17 14:42:20 -0500

    Content and styling

--- src/components/home/StayOnTop.vue
+++ src/components/home/StayOnTop.vue
@@ -1,6 +1,46 @@
 <template>
-  <v-img>
+  <v-img
+    :src="require('@/assets/blue_waves.png')"
+    fluid
+    class="pa-2"
+  >
+    <v-container
+      fluid
+      class="align-content-center fill-height"
+    >
+      <v-row
+        no-gutters
+        class="offset-lg-1"
+      >
+        <v-col
+          class="pa-6 col-md-7 col-lg-6"
+        >
+          <v-img
+            :src="require('@/assets/browsers.png')"
+          ></v-img>
+        </v-col>
+        <v-col
+          class="pa-6 col-md-5 col-lg-4 d-flex flex-column justify-center"
+          order="first"
+          order-md="last"
+        >
 
+          <p
+            class="text-h5 font-weight-bold"
+          >
+            Stay on top of what matters to you.
+          </p>
+
+          <p
+            class="text-body-1 font-weight-medium"
+          >
+            Managing Customers and keeping track of jobs is super easy to do
+          </p>
+
+        </v-col>
+      </v-row>
+    </v-container>
+    
   </v-img>
 </template>
 
