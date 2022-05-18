e5fcfae0ca159c92db3f7277cb4e5dfa300922c6
jothon <josh@inkbird.io>
2022-05-18 12:33:41 -0500

    More Optimizations

--- src/components/home/FeaturesPage.vue
+++ src/components/home/FeaturesPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-img id="features" :src="require('@/assets/features.png')">
+  <v-img id="features" :src="require('@/assets/features.png')" max-height="700">
     <v-container class="align-content-center fill-height">
       <v-card-title class="text-h5 font-weight-bold mb-2"
         >List of Features</v-card-title
@@ -11,50 +11,55 @@
         customers.
       </v-card-text>
       <v-row>
-        <v-col md="8">
-          <v-row class="d-flex flex-smAndDown-column align-center">
-            <v-col
-              v-for="(item, i) in features"
-              :key="i"
-              class="d-flex justify-center"
+        <v-col md="8" class="d-flex flex-column justify-center">
+          <div v-for="(item, i) in features" :key="i">
+            <v-btn
+              block
+              outlined
+              class="text-capitalize mb-3"
+              @click="carousel = i"
+              large
             >
-              <v-btn
-                width="100%"
-                outlined
-                class="text-capitalize"
-                @click="carousel = i"
-                large=""
-              >
-                {{ item.title }}
-                <v-spacer></v-spacer>
-                <v-icon>{{ item.icon }}</v-icon>
-              </v-btn>
-            </v-col>
-          </v-row>
-          <v-card-subtitle>
-            <v-icon>mdi-gesture-tap</v-icon>
-            Click on a feature for more info
-          </v-card-subtitle>
+              {{ item.title }}
+              <v-spacer></v-spacer>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-btn>
+          </div>
         </v-col>
 
-        <v-col md="4">
+        <v-col>
           <v-carousel
             vertical
             :show-arrows="false"
             hide-delimiters
             v-model="carousel"
             class="d-flex align-center"
+            height="350"
           >
             <v-carousel-item v-for="(item, i) in features" :key="i">
-              <v-card-title class="font-weight-bold">
-                {{ item.title }}
-              </v-card-title>
-              <v-card-text class="text-body-1">
-                {{ item.desc }}
-              </v-card-text>
+              <v-card
+                class="d-flex flex-column align-center justify-center"
+                outlined
+                height="100%"
+                min-width="100%"
+              >
+                <v-card-title class="font-weight-bold">
+                  {{ item.title }}
+                </v-card-title>
+                <v-icon>{{ item.icon }}</v-icon>
+                <v-card-text class="text-body-1 text-center">
+                  {{ item.desc }}
+                </v-card-text>
+              </v-card>
             </v-carousel-item>
           </v-carousel>
         </v-col>
+      </v-row>
+      <v-row>
+        <v-card-subtitle class="text-no-wrap">
+          <v-icon>mdi-gesture-tap</v-icon>
+          Click feature for more info
+        </v-card-subtitle>
       </v-row>
     </v-container>
   </v-img>
@@ -68,7 +73,7 @@ export default {
       features: [
         {
           title: "Scheduler",
-          desc: "Keep track of all jobs on a visual calendar, edit details and see who's doing what.",
+          desc: "Keep track of all jobs, edit details and see who's doing what.",
           icon: "mdi-calendar-blank",
         },
         {
@@ -93,7 +98,7 @@ export default {
         },
         {
           title: "Organize Clients",
-          desc: "Our system allows you to create Clients and add all thier details and quickly make job under their name",
+          desc: "Our system allows you to create Clients and add all thier details and quickly make jobs under their name",
           icon: "mdi-clipboard-account",
         },
       ],
