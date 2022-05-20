de9ed483b7d666ec59f40d76517dfeea821502d1
frostycoles <dcfrostad@gmail.com>
2022-05-20 13:03:44 -0500

    Adds transitions to cards

--- src/components/home/PricingPage.vue
+++ src/components/home/PricingPage.vue
@@ -1,6 +1,7 @@
 <template>
   <v-sheet
     color="background_dark"
+    v-scroll="revealCards"
   >
     <v-img
       id="pricing"
@@ -27,85 +28,104 @@
           class="justify-center"
         >
 
-          <v-card
-            class="ma-2 pa-3 d-flex flex-column"
-            max-width="350px"
-            min-height="500px"
+          <v-scale-transition
+            origin="bottom center"
           >
-
-            <v-card-title
-              class="justify-center font-weight-bold"
+            <v-card
+              v-if="reveal"
+              class="ma-2 pa-3 d-flex flex-column"
+              width="350px"
+              height="500px"
             >
-              Free Trial
-            </v-card-title>
 
-            <v-card-text
-              class="text-h5 justify-center text-center black--text font-weight-bold"
-            >
-              <span class="text-h4 font-weight-medium">$0</span> for 30 days
-            </v-card-text>
-
-            <v-card-text
-              class="px-9 text-body-1"
-            >
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-            </v-card-text>
-
-            <v-spacer></v-spacer>
-
-            <v-card-actions
-              class="justify-center"
-            >
-              <v-btn
-                x-large
-                color="primary"
-                width="100%"
+              <v-card-title
+                class="justify-center font-weight-bold"
               >
-                Get Started
-              </v-btn>
-            </v-card-actions>
+                Free Trial
+              </v-card-title>
 
-          </v-card>
+              <v-card-text
+                class="text-h5 justify-center text-center black--text font-weight-bold"
+              >
+                <span class="text-h4 font-weight-medium">$0</span> for 30 days
+              </v-card-text>
+
+              <v-card-text
+                class="px-9 text-body-1"
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+              </v-card-text>
+
+              <v-spacer></v-spacer>
+
+              <v-card-actions
+                class="justify-center"
+              >
+                <v-btn
+                  x-large
+                  color="primary"
+                  width="100%"
+                >
+                  Get Started
+                </v-btn>
+              </v-card-actions>
+
+            </v-card>
+          </v-scale-transition>
+
+          <v-scale-transition
+            origin="bottom center"
+          >
+            <v-card
+              v-if="reveal2"
+              class="ma-2 pa-3 d-flex flex-column"
+              width="350px"
+              height="500px"
+            >
+
+              <v-card-title
+                class="justify-center font-weight-bold"
+              >
+                All Features
+              </v-card-title>
+
+              <v-card-text
+                class="text-h5 justify-center text-center black--text font-weight-bold"
+              >
+                <span class="text-h4 font-weight-medium">$11</span> /month
+              </v-card-text>
+
+              <v-card-text
+                class="px-9 text-body-1"
+              >
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </v-card-text>
+
+              <v-spacer></v-spacer>
+
+              <v-card-actions
+                class="justify-center"
+              >
+                <v-btn
+                  x-large
+                  color="primary"
+                  width="100%"
+                >
+                  Get Started
+                </v-btn>
+              </v-card-actions>
+
+            </v-card>
+          </v-scale-transition>
 
           <v-card
+            v-if="!reveal2"
+            color="transparent"
+            flat
             class="ma-2 pa-3 d-flex flex-column"
-            max-width="350px"
-            min-height="550px"
-          >
-
-            <v-card-title
-              class="justify-center font-weight-bold"
-            >
-              All Features
-            </v-card-title>
-
-            <v-card-text
-              class="text-h5 justify-center text-center black--text font-weight-bold"
-            >
-              <span class="text-h4 font-weight-medium">$11</span> /month
-            </v-card-text>
-
-            <v-card-text
-              class="px-9 text-body-1"
-            >
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </v-card-text>
-
-            <v-spacer></v-spacer>
-
-            <v-card-actions
-              class="justify-center"
-            >
-              <v-btn
-                x-large
-                color="primary"
-                width="100%"
-              >
-                Get Started
-              </v-btn>
-            </v-card-actions>
-
-          </v-card>
+            width="350px"
+            height="550px"
+          ></v-card>
 
         </v-row>  
       </v-container>
@@ -116,6 +136,28 @@
 
 <script>
 export default {
-name: "pricingPage"
+name: "pricingPage",
+
+data() {
+  return {
+    reveal: false,
+    reveal2: false
+  }
+},
+
+methods: {
+  revealCards() {
+    var pricing = document.getElementById('pricing');
+    if(pricing.offsetTop < window.scrollY + (pricing.scrollHeight / 2)) {
+      this.reveal = true;
+      setTimeout(() => this.reveal2 = true, 300);
+    }
+  }
+},
+
+mounted() {
+  this.revealCards()
+}
+
 }
 </script>
