ee850eeb9a93454eddb441a9dc62fc82120fd311
jothon <josh@inkbird.io>
2022-05-23 16:49:30 -0500

    Adds Favicon and Page title

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+  transpileDependencies: ["vuetify"],
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "Amplify Lawns",
+    },
+  },
+});
